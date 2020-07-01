0022356d1ecfe6046fc3d74b05a7642433b95218
tapi0ka <takashi.kngw@gmail.com>
2020-07-01 21:25:06 +0900

    fix

--- bin/checkout.sh
+++ bin/checkout.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-set -a; . .env; set +a;
+# スクリプトの絶対パスを取得
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
+# .envから設定情報取得
+set -a; . ${SCRIPT_DIR}/.env; set +a;
 
 if [ -z "${REST_API_KEY}" ]; then
   REST_API_KEY="1234567890"
 fi
+echo "${REST_API_KEY}"
 
 curl -X POST -H "Authorization: Bearer ${REST_API_KEY}" localhost/api/jinjer/checkout
