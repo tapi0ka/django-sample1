d004bcb9c9335444c84ca6073ac6ffcad01386ff
tapi0ka <takashi.kngw@gmail.com>
2020-07-04 17:24:46 +0900

    環境構築shellをvirtualenvからvenvを使うように変更。

--- app/create_env.sh
+++ app/create_env.sh
@@ -7,19 +7,21 @@ echo "[$(date)] Start Shell."
 echo "Python開発環境を作成します"
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-ENV_DIR_NAME="$(basename `pwd`)-env"
+# ENV_DIR_NAME="$(basename `pwd`)-env"
+ENV_DIR_NAME="venv"
 
 if [ -d ${ENV_DIR_NAME} ]; then
   echo "環境がすでに存在しています"
 else
   echo "Pythonの仮想環境を作成します"
-  virtualenv ${ENV_DIR_NAME}
+  python3 -m venv ${ENV_DIR_NAME}
 fi
 
 . "./${ENV_DIR_NAME}/bin/activate"
 
 if [ -f "requirements.txt" ]; then
   echo "Pythonのパッケージをインストールします"
+  pip install wheel
   pip install -r requirements.txt
 fi
 
--- app/djangosample1/settings.py
+++ app/djangosample1/settings.py
@@ -24,6 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*a(vh07zqsy%cmq2@lv8a$k#66xx@p6eeaw9@kh8*667*97&&1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUGをTrueにすると/adminでログインし、テーブルの閲覧が可能になる
 DEBUG = False
 
 # ALLOWED_HOSTS = ['localhost', '127.0.0.1']
